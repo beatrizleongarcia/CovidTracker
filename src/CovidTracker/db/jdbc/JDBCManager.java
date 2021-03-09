301ecc7a34949b9bd131ab13aa74c1b7e5455fb7
carme <carme@10.60.58.72>
2021-03-09 13:02:30 +0100

    Create

--- src/CovidTracker/db/jdbc/JDBCManager.java
+++ src/CovidTracker/db/jdbc/JDBCManager.java
@@ -3,6 +3,7 @@ package CovidTracker.db.jdbc;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 import CovidTracker.db.ifaces.DBManager;
@@ -30,6 +31,74 @@ public class JDBCManager implements DBManager {
 		}
 	}
 
+	@Override
+	public void create() {
+	try {
+	// Create tables: begin
+	Statement stmt1 = c.createStatement();
+	String sql1 = "CREATE TABLE doctor(\n" 
+	+ "Id INTEGER PRIMARY KEY;\n" 
+	+ "name TEXT NOT NULL;\n"
+	+ "hospital TEXT NOT NULL;\n" 
+	+ ")";
+
+	stmt1.executeUpdate(sql1);
+	stmt1.close();
+	Statement stmt2 = c.createStatement();
+	String sql2 = "CREATE TABLE symptoms(\n"
+	    + "Id INTEGER PRIMARY KEY;\n"
+	+ "type TEXT NOT NULL;\n"
+	+ ")";
+	stmt2.executeUpdate(sql2);
+	stmt2.close();
+	Statement stmt3 = c.createStatement();
+	String sql3 = "CREATE TABLE quarantine(\n"
+	+ "id INTEGER PRIMARY KEY\n"
+	+ "time INTEGER NOT NULL\n"
+	+ "reason TEXT NOT NULL\n"
+	+ ")";
+	stmt3.executeUpdate(sql3);
+	stmt3.close();
+	Statement stmt4 = c.createStatement();
+	String sql4 = "CREATE TABLE patient(\n"
+	+ "id INTEGER PRIMARY KEY;\n"
+	+ "name TEXT NOT NULL;\n"
+	+ "date_birth DATE NOT NULL;\n"
+	+ "job_title TEXT NOT NULL;\n"
+	+ "Salary INTEGER NOT NULL;\n" 
+	+ "days_off_work INTEGER NOT NULL;\n"
+	+ "economic_impact INTEGER NOT NULL;\n"
+	+ "doctor_id INTEGER REFERENCES doctor(id)\n"
+	+ "symptoms_id INTEGER REFERENCES symptoms(id)\n"
+	+ "quarantine_id INTEGER REFERENCES quarantine(id)\n"
+	+ ")";
+	stmt4.executeUpdate(sql4);
+	stmt4.close();
+	Statement stmt5 = c.createStatement();
+	String sql5 = "CREATE TABLE covid_test(\n"
+	+ "Id INTEGER PRIMARY KEY;\n"
+	+ "date_of_test DATE NOT NULL;\n"
+	+ "price REAL NOT NULL;\n"
+	    + "laboratory TEXT NOT NULL;\n"
+	+ "type TEXT NOT NULL;\n"
+	+ "pb_pv TEXT NOT NULL;\n"
+	+ "patient_id INTEGER REFERENCES patient(id)\n"
+	+ ")";
+	stmt5.executeUpdate(sql5);
+	stmt5.close();
+	Statement stmt6 = c.createStatement();
+	String sql6 = "CREATE TABLE patient_symptoms(\n"
+	+ "patient_id INTEGER REFERENCES patient(id)\n"
+	+ "contact_id INTEGER REFERENCES patient(id)\n"
+	+ ")";
+	stmt6.executeUpdate(sql6);
+	stmt6.close();
+	System.out.println("Tables created.");
+	} catch (Exception e) {
+	e.printStackTrace();
+	}
+
+	}
 	@Override
 	public void disconnect() {
 		// TODO Auto-generated method stub
