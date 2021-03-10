cc21b4d0177ad2013952464553d50fc0e387878a
beatr <beatr@10.60.83.49>
2021-03-10 19:04:41 +0100

    menu

--- src/CovidTracker/ui/Menu.java
+++ src/CovidTracker/ui/Menu.java
@@ -1,12 +1,13 @@
 package CovidTracker.ui;
 
 import CovidTracker.db.ifaces.DBManager;
+import CovidTracker.db.jdbc.JDBCManager;
 
 public class Menu {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		DBManager man = new DBManager();
+		DBManager man = new JDBCManager();
 		man.connect();
 	}
 
