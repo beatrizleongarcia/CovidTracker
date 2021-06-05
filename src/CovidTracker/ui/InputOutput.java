41a3a4f2e32271e7617c4b56f666e121ed4174c5
carme <carme@192.168.1.55>
2021-06-06 01:31:33 +0200

    Cambios

--- src/CovidTracker/ui/InputOutput.java
+++ src/CovidTracker/ui/InputOutput.java
@@ -3,31 +3,20 @@ package CovidTracker.ui;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.sql.Date;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
-
-import CovidTracker.db.jdbc.JDBCManager;
-import CovidTracker.db.jpa.JPAUserManager;
 import CovidTracker.db.pojos.Covid_Test;
 import CovidTracker.db.pojos.Doctor;
 import CovidTracker.db.pojos.Patient;
 import CovidTracker.db.pojos.Quarantine;
 import CovidTracker.db.pojos.Symptoms;
-import CovidTracker.db.pojos.users.Role;
-import CovidTracker.db.pojos.users.User;
-import CovidTracker.db.ifaces.DBManager;
-import jdk.jshell.execution.Util;
+
 
 public class InputOutput {
 
 	private static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-
 
 	public static Patient addPatient() {
 		List<Symptoms> symptoms = new ArrayList<Symptoms>();
@@ -42,25 +31,25 @@ public class InputOutput {
 			String job_title = get_String();
 			System.out.println("Salary");
 			Float salary = get_Float();
-			
+
 			System.out.println("How many symptoms does the patient have? (0 for no synmptoms)");
 			Integer number_symptoms = get_int();
-			if(number_symptoms>0) {
-			for (int j = 0; j < number_symptoms; j++) {
-				Symptoms symp = addSymptoms();
-				symptoms.add(symp);
-			}}
+			if (number_symptoms > 0) {
+				for (int j = 0; j < number_symptoms; j++) {
+					Symptoms symp = addSymptoms();
+					symptoms.add(symp);
+				}
+			}
 			System.out.println("How many quarantines the patient has been in?");
 			Integer number_qua = get_int();
 			for (int j = 0; j < number_qua; j++) {
-				Quarantine qua  = addQuarantine();
+				Quarantine qua = addQuarantine();
 				quarantines.add(qua);
 			}
-			
-		
-			Patient pat = new Patient(name, Date.valueOf(date), job_title, salary, symptoms,quarantines);
+
+			Patient pat = new Patient(name, Date.valueOf(date), job_title, salary, symptoms, quarantines);
 			return pat;
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -68,34 +57,35 @@ public class InputOutput {
 		return null;
 
 	}
-	
-	 public static int getIntBetweenRange(String message, int begin, int end) {
-	        boolean fechaCorrecta = false;
-	        int num = -1;
-	        while (!fechaCorrecta) {
-	            System.out.println(message);
-	            try {
-	                num = Integer.parseInt(in.readLine());
-	                if (!(num < begin || num > end)) {
-	                    fechaCorrecta = true;
-	                } else {
-	                    System.err.println("Wrong number, try again");
-	                }
-	            } catch (NumberFormatException | IOException e) {
-	                System.err.println("It must be an integer");
-	            }
-	        }
 
-	        return num;
-	    }
-	
-	 public static LocalDate createDate() {
-	        int dia = getIntBetweenRange("Insert the day of the month", 1, 31);
-	        int mes = getIntBetweenRange("Insert  the month", 1, 12);
-	        int anyo = getIntBetweenRange("Insert the year", 1900, 2021);
-	        LocalDate fecha = LocalDate.of(anyo, mes, dia);
-	        return fecha;
-	    }
+	public static int getIntBetweenRange(String message, int begin, int end) {
+		boolean fechaCorrecta = false;
+		int num = -1;
+		while (!fechaCorrecta) {
+			System.out.println(message);
+			try {
+				num = Integer.parseInt(in.readLine());
+				if (!(num < begin || num > end)) {
+					fechaCorrecta = true;
+				} else {
+					System.err.println("Wrong number, try again");
+				}
+			} catch (NumberFormatException | IOException e) {
+				System.err.println("It must be an integer");
+			}
+		}
+
+		return num;
+	}
+
+	public static LocalDate createDate() {
+		int dia = getIntBetweenRange("Insert the day of the month", 1, 31);
+		int mes = getIntBetweenRange("Insert  the month", 1, 12);
+		int anyo = getIntBetweenRange("Insert the year", 1900, 2021);
+		LocalDate fecha = LocalDate.of(anyo, mes, dia);
+		return fecha;
+	}
+
 	public static Doctor addDoctor() {
 		try {
 			System.out.println("Introduce a new doctor");
@@ -103,9 +93,9 @@ public class InputOutput {
 			String name = get_String();
 			System.out.println("Hospital:");
 			String hospital = get_String();
-			Doctor doc = new Doctor (name, hospital);
+			Doctor doc = new Doctor(name, hospital);
 			return doc;
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -113,6 +103,7 @@ public class InputOutput {
 		return null;
 
 	}
+
 	private static Quarantine addQuarantine() {
 		try {
 			while (true) {
@@ -128,17 +119,17 @@ public class InputOutput {
 				switch (opcion) {
 				case 1:
 					String type = "contact";
-					return new Quarantine(type,1);
+					return new Quarantine(type, 1);
 				case 2:
 					type = "symptoms";
-					return new Quarantine(type,2);
-					
+					return new Quarantine(type, 2);
+
 				case 3:
 					type = "confirmed";
-					return new Quarantine(type,3);
+					return new Quarantine(type, 3);
 				case 4:
 					type = "Routine test";
-					return new Quarantine(type,4);
+					return new Quarantine(type, 4);
 
 				}
 			}
@@ -155,15 +146,26 @@ public class InputOutput {
 			System.out.println("Complete the information about the covid test");
 			System.out.println("Public or private test:");
 			String public_private = get_String();
+			while (!public_private.equalsIgnoreCase("public") && !public_private.equalsIgnoreCase("private")) {
+				System.out.println("Incorrectly entered data");
+				System.out.println("Public or private test:");
+				public_private = get_String();
+			}
 			System.out.println("Type of test(pcr,rapid test or antibody test)");
 			String type_test = get_String();
+			while (!type_test.equalsIgnoreCase("pcr") && !type_test.equalsIgnoreCase("rapid test")
+					&& !type_test.equalsIgnoreCase("antibody test")) {
+				System.out.println("Incorrectly entered data");
+				System.out.println("Type of test(pcr,rapid test or antibody test)");
+				type_test = get_String();
+			}
 			System.out.println("Date of the test:");
 			LocalDate date = createDate();
 			System.out.println("Price of the test:");
 			Float price = get_Float();
 			System.out.println("Laboratory");
 			String laboratory = get_String();
-			tests = new Covid_Test(public_private, type_test, Date.valueOf(date), price,laboratory);
+			tests = new Covid_Test(public_private, type_test, Date.valueOf(date), price, laboratory);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -187,22 +189,22 @@ public class InputOutput {
 				switch (opcion) {
 				case 1:
 					String type = "Fever";
-					return new Symptoms(type,1);
+					return new Symptoms(type, 1);
 				case 2:
 					type = "Dry cough";
-					return new Symptoms(type,2);
+					return new Symptoms(type, 2);
 				case 3:
 					type = "Tireness";
-					return new Symptoms(type,3);
+					return new Symptoms(type, 3);
 				case 4:
 					type = "Ache and pains";
-					return new Symptoms(type,4);
+					return new Symptoms(type, 4);
 				case 5:
 					type = "Diarrhoea";
-					return new Symptoms(type,5);
+					return new Symptoms(type, 5);
 				case 6:
 					type = "Loss taste and smell";
-					return new Symptoms(type,6);
+					return new Symptoms(type, 6);
 				}
 			}
 
@@ -259,6 +261,7 @@ public class InputOutput {
 		}
 		return a;
 	}
+
 	public static String getDocfromKeyboard() {
 		String a = null;
 		try {
@@ -269,6 +272,5 @@ public class InputOutput {
 		}
 		return a;
 	}
-	
 
 }
